a6e694cc2e3534143578c907e44e7f0c1038020d
famkepj <82056762+famkepj@users.noreply.github.com>
2021-11-19 15:35:50 +0100

    Update script.js

--- script.js
+++ script.js
@@ -1,10 +1,12 @@
 async function getData(){
     try{    
-        const apiUrl =  "https://api.themoviedb.org/3/genre/movie/list?api_key="+api_key;
+        // const apiUrl =  "https://api.themoviedb.org/3/genre/movie/list?api_key=${api_key}";
+        const apiUrl =  "https://api.themoviedb.org/3/genre/movie/list?api_key=356e96dc3634b34c274d78d0873db8be";
         var res = await fetch(apiUrl);
-        return await res.json();
-    } catch (err){
-        console.log("error")
+        var genre = await res.json();
+        console.log (genre);
+    } catch(err){
+    console.log("error")
     }
 }
 
